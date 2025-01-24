367461987a0ac3aa6d1f2bf6c0ca45ac7fed3083
Black 2.o <black2.o@yahoo.com>
2025-01-25 00:56:33 +0600

    Added a code From Gpt

--- StonesontheTable.cpp
+++ StonesontheTable.cpp
@@ -1,23 +1,48 @@
-#include <iostream>
+// #include <iostream>
 
+
+// int main() {
+//     int n;
+//     char colors[n];
+//     int x = 0;
+
+//     std::cin >> n;
+
+//     for (int i = 0; i < n; i++) {
+//         std::cin >> colors[i];
+//     }
+
+//     for (int i = 0; i < n; i++) {
+//         if(colors[i] == colors[i+1]){
+//             x++;
+//         }
+//     }
+//     std::cout << x;
+
+//     return 0;
+// }
+
+#include <iostream>
+#include <string>
+using namespace std;
 
 int main() {
     int n;
-    char colors[n];
-    int x = 0;
+    cin >> n; // Input the number of stones
+    string s;
+    cin >> s; // Input the string representing the stones' colors
 
-    std::cin >> n;
+    int remove_count = 0;
 
-    for (int i = 0; i < n; i++) {
-        std::cin >> colors[i];
-    }
-
-    for (int i = 0; i < n; i++) {
-        if(colors[i] == colors[i+1]){
-            x++;
+    // Iterate through the stones
+    for (int i = 0; i < n - 1; i++) {
+        if (s[i] == s[i + 1]) {
+            remove_count++; // Increment count if two neighboring stones have the same color
         }
     }
-    std::cout << x;
+
+    // Output the result
+    cout << remove_count << endl;
 
     return 0;
 }
