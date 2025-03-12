e5c29ce8688f5470b4d04868d08c4556d81dc599
Black 2.o <black2.o@yahoo.com>
2025-03-12 19:38:19 +0600

    Recursion 2nd Video Done

--- Take_YOU_Forward/Recursion.cpp
+++ Take_YOU_Forward/Recursion.cpp
@@ -17,12 +17,34 @@ void r1(int i, int n){
     i++;
     r1(i, n);
 }
+void r2(int i, int n){
+    // if(i >= n) return;
+    // // cout << "Black" << "\n";
+    // cout << i+1 << "\n";
+    // i++;
+    // r2(i, n);
 
+
+    // if(n < 1) return;
+    // cout << n << "\n";
+    // n--;
+    // r2(i, n);
+
+    // // Backtracking
+    // if(i < 1) return;
+    // r2(i-1, n);
+    // cout << i << "\n";
+
+    // Backtracking
+    if(i > n) return;
+    r2(i+1, n);
+    cout << i << "\n";
+}
 
 int main() {
     int n;
     cin >> n;
     int i = 0;
-    r1(i, n);
+    r2(i, n);
     return 0;
 }
