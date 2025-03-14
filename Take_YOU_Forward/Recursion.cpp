86ff98fb0689baa645f563639e58c59f3fa9ae84
Black 2.o <black2.o@yahoo.com>
2025-03-14 15:00:35 +0600

    Re 4. Problems on Functional Recursion | Strivers A2Z DSA Course

--- Take_YOU_Forward/Recursion.cpp
+++ Take_YOU_Forward/Recursion.cpp
@@ -2,39 +2,48 @@
 using namespace std;
 
 
+// Re 4. Problems on Functional Recursion | Strivers A2Z DSA Course
+// Reverse an Array 
+// Palindrome a Word 
 
-void Parameterised(int n, int sum){
-    // Parameterised Way 
-    if(n < 1){
-        cout << sum;
-        return;
+void reverseArray(int arr[], int start, int end){
+    if(end <= start) return;
+    swap(arr[start], arr[end]);
+    reverseArray(arr, start+1, end-1);
+}
+
+void anotherReverseArray(int arr[], int i, int n){
+    if(i >= n/2) return;
+    swap(arr[i], arr[n-i-1]);
+    anotherReverseArray(arr, i+1, n);
+}
+
+bool palindrome(string str, int i, int n){
+    if(i >= n/2) return true;
+
+    if(palindrome(str, i+1, n)){
+        if(str[i] == str[n-i-1]) return true;
+        else return false;
+    }else{
+        return false;
     }
-    Parameterised(n-1, sum+n);
 }
-
-int functional(int n){
-    if(n == 0) return 0;
-    
-    return n + functional(n-1);
-
-}
-
-int factorial(int n){
-    if(n == 1) return 1;
-    return n* factorial(n-1);
-}
-
 
 int main() {
-    int n;
-    cin >> n;
-    int i = 0;
-    // Parameterised(n, i);
-    // int sum = functional(n);    
-    // cout << sum;
+    // int n;
+    // cin >> n;
+    // // int i = 0;
+    // int arr[n];
+    // for(int i = 0; i < n; i++) cin >> arr[i];
+    // // reverseArray(arr, 0, n-1);
+    // anotherReverseArray(arr, 0, n);
+    // for(int i = 0; i < n; i++) cout << arr[i] << " ";
 
-    int factorialValue = factorial(n);
-    cout << factorialValue;
+    string str;
+    cin >> str;
+    int len = str.length();
+    bool ispa = palindrome(str, 0, len);
+    cout << ispa;
 
     return 0;
 }
