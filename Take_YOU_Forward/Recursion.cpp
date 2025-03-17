48cce8db44d519917385354f90967fa70112b12f
Black 2.o <black2.o@yahoo.com>
2025-03-17 18:47:23 +0600

    Lec 5: Learn Basic Recursion Done I will Do the next Recursion later

--- Take_YOU_Forward/Recursion.cpp
+++ Take_YOU_Forward/Recursion.cpp
@@ -2,37 +2,16 @@
 using namespace std;
 
 
-// * Fibonacci - Re 5. Multiple Recursion Calls | Problems | Strivers A2Z DSA Course
+// L6. Recursion on Subsequences | Printing Subsequences
 
-// Exponcial 2^n  near this.
-
-void fibonnacciforloop(int n){
-    int a = 0;
-    int b = 1;
-    int num = 0;
-    for(int i = 0; i < n-1; i++){
-        num = a + b;
-        a = b;
-        b = num;
-    }
-    cout << num;
-}
-
-int fibonacci(int n){
-    if(n <= 1) return n;
-
-    return fibonacci(n-1) + fibonacci(n-2);
-}
 
 
 
 int main() {
     int n;
     cin >> n;
-
-    // fibonnacciforloop(n);
-
-    cout << fibonacci(n);
+    int arr[n];
+    for(int i = 0;i < n; i++) cin >> arr[i];
 
     return 0;
 }
