dd181335bb00de3b31a31cc793e6d0179f7a0cb0
Black 2.o <black2.o@yahoo.com>
2026-02-21 00:31:27 +0600

    Humurgers Problem Solved First 1600 Ranked Problem

--- Strivers Sheet/BinarySearch/Hamburgers.cpp
+++ Strivers Sheet/BinarySearch/Hamburgers.cpp
@@ -24,37 +24,57 @@ int main(){
         }
     }
 
-    while(nb >= b and ns >= s and nc >= c){
-        cout << "NB " << nb << " NS " << ns << " NC " << nc << endl;
-        nb-=b; ns-=s; nc-=c;
+
+    while(nb > 0 or ns > 0 or nc > 0){
+        // cout << "NB " << nb << " NS " << ns << " NC " << nc << endl;
+        if(b > 0){
+            if(nb >= b){
+                nb -= b;
+            }else{
+                r -= abs(b-nb) * pb;
+                nb = 0;
+                if(r >= 0){
+
+                }else{
+                    cout << ans;
+                    return 0;
+                }
+            }
+        }else{nb = 0;}
+        if(s > 0){
+            if(ns >= s){
+                ns -= s;
+            }else{
+                r -= abs(s-ns) * ps;
+                ns = 0;
+                if(r >= 0){
+
+                }else{
+                    cout << ans;
+                    return 0;
+                }
+            }
+        }else{ns = 0;}
+        if(c > 0){
+            if(nc >= c){
+                nc -= c;
+            }else{
+                r -= abs(c-nc) * pc;
+                nc = 0;
+                if(r >= 0){
+
+                }else{
+                    cout << ans;
+                    return 0;
+                }
+            }
+        }else{nc = 0;}
+
         ans+=1;
     }
 
-    int nb_need = 0;
-    int ns_need = 0;
-    int nc_need = 0;
 
-    if(b > 0 and nb > 0){nb_need = b - nb;}
-    if(s > 0 and ns > 0){ns_need = s - ns;}
-    if(c > 0 and nc > 0){nc_need = c - nc;}
-
-    cout << "NB " << nb << " NS " << ns << " NC " << nc << endl;
-    
-    cout << "NB Need " << nb_need << " NS Need " << ns_need << " NC Need " << nc_need<< endl;
-
-
-    int need_price = (nb_need * pb) + (ns_need * ps) + (nc_need * pc);
-    cout << need_price << endl;
-    if(r > need_price and need_price > 0){
-        r-= need_price;
-        ans+=1;
-    }
-
-        
-        // cout << ans << endl;
     int one_f_price = ( b * pb ) + ( s * ps ) + ( c * pc );
-        // cout << "one FUll Price " << one_f_price << " r " << r << endl;
-        // cout << ".. " <<  (r / one_f_price) << endl;
     ans += (r / one_f_price);
 
 
